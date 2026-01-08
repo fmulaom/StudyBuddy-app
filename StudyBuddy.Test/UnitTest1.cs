2f2b4343e5aad130a6eae90dc29c48d91e55a2d6
pblaze77 <pblaze3@algebra.hr>
2026-01-08 16:48:37 +0100

    Singleton i Strategy testovi

--- StudyBuddy.Test/UnitTest1.cs
+++ StudyBuddy.Test/UnitTest1.cs
@@ -1,11 +1,54 @@
+using System;
+using StudyBuddy.Web.Models;
+using StudyBuddy.Web.Services.Interfaces;
+using StudyBuddy.Web.Services.LearningGoalConfig;
 namespace StudyBuddy.Test
+
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly ILearningGoalProgressStrategy _strategy = new DeadlineAwareProgressStrategy();
 
+        [Fact]
+        public void PastDeadline_IncreasesProgressBy10()
+        {
+            var goal = new LearningGoal
+            {
+                Progress = 50,
+                TargetDate = DateTime.Today.AddDays(-1)
+            };
+
+            var result = _strategy.CalculateProgress(goal);
+
+            Assert.Equal(60, result);
+        }
+
+        [Fact]
+        public void FutureDeadline_KeepsProgressSame()
+        {
+            var goal = new LearningGoal
+            {
+                Progress = 50,
+                TargetDate = DateTime.Today.AddDays(5)
+            };
+
+            var result = _strategy.CalculateProgress(goal);
+
+            Assert.Equal(50, result);
+        }
+
+        [Fact]
+        public void PastDeadline_DoesNotExceed100()
+        {
+            var goal = new LearningGoal
+            {
+                Progress = 95,
+                TargetDate = DateTime.Today.AddDays(-1)
+            };
+
+            var result = _strategy.CalculateProgress(goal);
+
+            Assert.Equal(100, result);
         }
     }
 }
